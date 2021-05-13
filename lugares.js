8ea894127218cb1e705a870e93d11a759e5bedc1
Rodrigo Baltazar <rodrigobaltazarinf@gmail.com>
2021-05-12 22:17:09 -0300

    lugares do BR quebram linha e estragam caixa que estava certa, retirar para arrumar

---
+++ lugares.js
@@ -0,0 +1,20 @@
+var Brasil = [norte, nordeste, sudeste, centroOeste, sul];
+
+var norte = ["Acre", "Amazonas", "Amapá", "Pará", "Roraima", "Rondônia", "Tocantins"];
+
+var nordeste = ["Bahia","Sergipe","Alagoas","Pernambuco","Paraíba","Rio Grande do Norte","Ceará","Maranhão","Piauí"];
+
+var sudeste = ["São Paulo","Minas Gerais","Espírito Santo","Rio de Janeiro"];
+
+var centroOeste = ["Mato Grosso","Mato Grosso do Sul","Goiás","Distrito Federal"];
+
+var sul = ["Rio Grande do Sul","Santa Catarina","Paraná"];
+
+document.getElementById("regiaoNorte").innerHTML = norte;
+document.getElementById("regiaoNordeste").innerHTML = nordeste;
+document.getElementById("regiaoSudeste").innerHTML = sudeste;
+document.getElementById("regiaoCentroOeste").innerHTML = centroOeste;
+document.getElementById("regiaoSul").innerHTML = sul;
+
+
+
